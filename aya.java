dd9fe3b70abcc8852d200eecb05230c346ad246a
ayamagdy980 <ayam464214@gmail.com>
2021-11-14 17:58:20 +0200

    aya

---
+++ aya.java
@@ -0,0 +1,7 @@
+class aya {
+    public static void main(String args[]){
+           System.out.println("Aya Magdy Fayed");
+            System.out.println("Section 1");
+             System.out.println("cs");
+    }
+}
